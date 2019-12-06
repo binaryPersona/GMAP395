2a03c45665a0c5ce8d299bebd7a27928f15596f0
Zoe <sorafnlform@gmail.com>
2019-12-06 02:47:34 -0500

    Rhythm works, targets almoossttt move

--- GMAP395_Final/Assets/Scripts/RhythmTracker.cs
+++ GMAP395_Final/Assets/Scripts/RhythmTracker.cs
@@ -6,25 +6,51 @@ public class RhythmTracker : MonoBehaviour
 {
     [SerializeField]
     protected float bpm;
+    [SerializeField]
+    protected int beatsShownInAdvance;
+    [SerializeField]
+    protected GameObject targetPrefab;
+    [SerializeField]
+    protected Transform spawnPosition;
+    [SerializeField]
+    protected Transform despawnPosition;
 
     private float secPerBeat;
-    private float dspTime;
     private float songPosition;
+    private float songPositionInBeats;
+    private float dspSongTime;
+    private float[] beats = new float[] { 2f, 4f, 6f, 8f, 10f, 12f, 14f, 16f, 18f, 20f, 50f };
+    private int nextIndex = 0;
+
+    private AudioSource musicSource;
 
     // Start is called before the first frame update
     void Start()
     {
+        musicSource = GetComponent<AudioSource>();
         secPerBeat = 60f / bpm;
-        dspTime = (float)AudioSettings.dspTime;
-        GetComponent<AudioSource>().Play();
+        dspSongTime = (float)AudioSettings.dspTime;
+        musicSource.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
-        songPosition = (float)(AudioSettings.dspTime - dspTime);
+        songPosition = (float)(AudioSettings.dspTime - dspSongTime);
 
         //calculate the position in beats
-        // songPosInBeats = songPosition / secPerBeat;
+        songPositionInBeats = songPosition / secPerBeat;
+
+        if (nextIndex < beats.Length && beats[nextIndex] < songPositionInBeats + beatsShownInAdvance)
+        {
+            GameObject targetObject = Instantiate(targetPrefab, spawnPosition.position, spawnPosition.rotation);
+            Target target = targetObject.GetComponent<Target>();
+            target.beatsShownInAdvance = beatsShownInAdvance;
+            target.songPositionInBeats = songPositionInBeats;
+            target.beatOfThisNote = beats[nextIndex];
+            target.spawnPosition = spawnPosition;
+            target.despawnPosition = despawnPosition;
+            nextIndex++;
+        }
     }
 }
--- GMAP395_Final/Assets/Scripts/Target.cs
+++ GMAP395_Final/Assets/Scripts/Target.cs
@@ -2,22 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Target : MonoBehaviour {
+public class Target : MonoBehaviour
+{
     [SerializeField]
     protected int currentHealth = 1;
     [SerializeField]
     protected float despawnTime = 1.2f;
+    [SerializeField]
+    public Transform spawnPosition;
+    [SerializeField]
+    public Transform despawnPosition;
 
-    public void Damage (int damageAmount) {
+    public int beatsShownInAdvance;
+    public float beatOfThisNote;
+    public float songPositionInBeats;
+
+    void Update()
+    {
+        Debug.Log((beatsShownInAdvance - (beatOfThisNote - songPositionInBeats)) / beatsShownInAdvance);
+        transform.position = Vector3.Lerp(spawnPosition.position, despawnPosition.position, (beatsShownInAdvance - (beatOfThisNote - songPositionInBeats)) / beatsShownInAdvance);
+    }
+
+    public void Damage(int damageAmount)
+    {
         currentHealth -= damageAmount;
 
-        if (currentHealth <= 0) {
-            StartCoroutine (Destroy ());
+        if (currentHealth <= 0)
+        {
+            StartCoroutine(Destroy());
         }
     }
 
-    private IEnumerator Destroy () {
+    private IEnumerator Destroy()
+    {
         yield return despawnTime;
-        gameObject.SetActive (false);
+        gameObject.SetActive(false);
     }
 }
