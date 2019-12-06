69510cd33fd86642ada69885f50ef75ac1fdf5f0
Zoe <sorafnlform@gmail.com>
2019-12-05 22:39:48 -0500

    It's progress at least

--- GMAP395_Final/Assets/Scripts/GunShoot.cs
+++ GMAP395_Final/Assets/Scripts/GunShoot.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GunShoot : MonoBehaviour {
+public class GunShoot : MonoBehaviour
+{
     [SerializeField]
     protected int baseDamage = 1;
     [SerializeField]
@@ -19,41 +20,50 @@ public class GunShoot : MonoBehaviour {
     private LineRenderer laserLine;
     private float nextFire;
 
-    private WaitForSeconds shotDuration = new WaitForSeconds (0.07f);
+    private WaitForSeconds shotDuration = new WaitForSeconds(0.07f);
 
     // Start is called before the first frame update
-    void Start () {
-        laserLine = GetComponent<LineRenderer> ();
-        gunAudio = GetComponent<AudioSource> ();
-        fpsCam = GetComponentInParent<Camera> ();
+    void Start()
+    {
+        laserLine = GetComponent<LineRenderer>();
+        // gunAudio = GetComponent<AudioSource> ();
+        fpsCam = GetComponentInParent<Camera>();
     }
 
     // Update is called once per frame
-    void Update () {
-        if (Input.GetButtonDown ("Fire1") && Time.time > nextFire) {
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && Time.time > nextFire)
+        {
             nextFire = Time.time + fireRate;
-            StartCoroutine (ShotEffect ());
-            Vector3 rayOrigin = fpsCam.ViewportToWorldPoint (new Vector3 (0.5f, 0.5f, 0.0f));
+            StartCoroutine(ShotEffect());
+            Vector3 rayOrigin = fpsCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.0f));
             RaycastHit hit;
-            laserLine.SetPosition (0, gunMuzzle.position);
-            if (Physics.Raycast (rayOrigin, fpsCam.transform.forward, out hit, range)) {
-                laserLine.SetPosition (1, hit.point);
-                Target health = hit.collider.GetComponent<Target> ();
+            laserLine.SetPosition(0, gunMuzzle.position);
+            if (Physics.Raycast(rayOrigin, fpsCam.transform.forward, out hit, range))
+            {
+                laserLine.SetPosition(1, hit.point);
+                Target health = hit.collider.GetComponent<Target>();
                 //TODO: add varying damage based off of beat
-                if (health != null) {
-                    health.Damage (baseDamage);
+                if (health != null)
+                {
+                    health.Damage(baseDamage);
                 }
-                if (hit.rigidbody != null) {
-                    hit.rigidbody.AddForce (-hit.normal * hitForce);
+                if (hit.rigidbody != null)
+                {
+                    hit.rigidbody.AddForce(-hit.normal * hitForce);
                 }
-            } else {
-                laserLine.SetPosition (1, rayOrigin + (fpsCam.transform.forward * range));
+            }
+            else
+            {
+                laserLine.SetPosition(1, rayOrigin + (fpsCam.transform.forward * range));
             }
         }
     }
 
-    private IEnumerator ShotEffect () {
-        gunAudio.Play ();
+    private IEnumerator ShotEffect()
+    {
+        // gunAudio.Play ();
         laserLine.enabled = true;
         yield return shotDuration;
         laserLine.enabled = false;
---
+++ GMAP395_Final/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float lookSensitivity = 2f, lookSmoothDamp = 0.5f;
+    private float yRot, xRot;
+    private float currentY, currentX;
+    private float yRotationV, xRotationV;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void LateUpdate()
+    {
+        yRot += Input.GetAxis("Mouse X") * lookSensitivity;
+        xRot += Input.GetAxis("Mouse Y") * lookSensitivity;
+
+        currentX = Mathf.SmoothDamp(currentX, xRot, ref xRotationV, lookSmoothDamp);
+        currentY = Mathf.SmoothDamp(currentY, yRot, ref yRotationV, lookSmoothDamp);
+
+        // yRot = Mathf.Clamp(yRot, -80, 80);
+        xRot = Mathf.Clamp(xRot, -80, 80);
+
+        transform.rotation = Quaternion.Euler(-currentX, currentY, 0);
+    }
+}
